41d2dce79ffb6ee8aaea03ac514d5f76efd5cbf1
Atripat19 <aditya_t@hcl.com>
2024-07-18 21:16:34 +0530

    Gmail form basic structure and validations

--- src/app/forms/gmail-signup24/gmail-signup24.component.ts
+++ src/app/forms/gmail-signup24/gmail-signup24.component.ts
@@ -8,32 +8,76 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class GmailSignup24Component implements OnInit {
 
+  monthArr = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  genders = ['Male', 'Female', 'Rather not say', 'Custom'];
+  existingEmailList = ['amptripathi@gmail.com', 'test@gmail.com'];
+
+  fields: [];
+  genderModel: {
+    whatIsYourGender: '',
+    referToMeAs: '',
+  }
+
   gmailForm = new FormGroup({
-    firstName: new FormControl(null, Validators.required),
-    lastName: new FormControl(null, Validators.required),
+    completeName : new FormGroup({
+      firstName: new FormControl(null, Validators.required),
+      lastName: new FormControl(null, Validators.required),
+    }),
     dateOfBirth: new FormGroup({
-      month: new FormControl(null, Validators.maxLength(2)),
-      day: new FormControl(null, Validators.maxLength(2)),
-      year: new FormControl(null, Validators.maxLength(4)),
+      month: new FormControl(null),
+      day: new FormControl(null, [Validators.required, Validators.maxLength(2)]),
+      year: new FormControl(null, [Validators.required, Validators.maxLength(4)]),
     }),
     gender: new FormControl(null),
     // add form controls for custom gender post selection in gender.
-    email: new FormControl(null, Validators.email),
-    password: new FormControl(null),
-    passwordConfirm: new FormControl(null),
+    email: new FormControl(null, [Validators.required ,Validators.email], this.checkExistingEmail.bind(this)),
+    password: new FormControl(null, Validators.required),
+    passwordConfirm: new FormControl(null, Validators.required),
     countryPinAndMobile: new FormGroup({
-      countryPin: new FormControl(null, Validators.required),
-      mobileNumber: new FormControl(null, Validators.required)
+      countryPin: new FormControl(null, [Validators.required, this.isNumbersOnly]),
+      mobileNumber: new FormControl(null, [Validators.required, this.isNumbersOnly, Validators.maxLength(10), Validators.minLength(10)])
     }),
-    confirmCode: new FormControl(null, [Validators.required, Validators.maxLength(6)])
+    confirmCode: new FormControl(null, [Validators.required, this.isNumbersOnly, Validators.maxLength(6), Validators.minLength(6)])
   });
 
-  ngOnInit(): void {
-      
+  ngOnInit(): void {}
+
+  onGenderSelect() {
+    switch (this.gmailForm.get('gender').value) {
+      case 'Custom' : {
+        // this.gmailForm.addControl(this, 'whatIsYourGender', new FormControl(null, Validators.required))
+      } 
+    }
+  }
+
+  async checkExistingEmail(formControl: FormControl): Promise<{[key: string]: boolean}> {
+    if (this.existingEmailList.includes(formControl.value)) {
+      return {existingEmail: true};
+    }
+    return null;
+    // return new Promise((resolve, reject)=> {
+    //   setTimeout(()=> {
+    //     if (this.existingEmailList.includes(formControl.value)) {
+    //       resolve({existingEmail: true})
+    //     }
+    //     resolve(null);
+    //   }, 2000);
+    // });
+  }
+
+  isNumbersOnly(formControl: FormControl): {[key: string]: boolean} {
+    console.log(formControl.value, +formControl.value, isNaN(+formControl.value));
+    if (isNaN(+formControl.value)) {
+      return { 
+        'isAlphabet': true 
+      };
+    }
+    else return null;
   }
 
   onSubmit() {
-
+    console.log(this.gmailForm);
+    console.log(this.gmailForm.get(''));
   }
 }
 // Gmail Signup Form
